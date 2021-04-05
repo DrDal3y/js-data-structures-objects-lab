ceddc072888e237ff9c91e5d94e3f21b5cdedcd0
Marcus <marcus.daley1@outlook.com>
2021-04-04 23:06:20 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const driver = {
+  name: 'Sam',
+  address: '11 Broadway'
+};
+
+function updateDriverWithKeyAndValue(driver, name, value) {
+  // Alternate using ES6 Spread operators:
+  // return { ...driver, ...{ [key]: value } }
+//   return Object.assign({}, driver, { [key]: value });
+return Object.assign({}, driver, { [name]: value});
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  // Alternate using ES6 Spread operators:
+  // const newObj = { ...driver }
+  const newdriver = Object.assign({}, driver);
+
+  delete newdriver[key];
+
+  return newdriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+
+  return driver;
+}
